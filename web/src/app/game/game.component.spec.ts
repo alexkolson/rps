f931ce9b37b69ee972e523e75aba0578e8fb027f
Alex Olson <alexkellenolson@gmail.com>
2022-08-01 02:51:09 +0200

    add httpclienttestingmodule to statistics and game component

--- web/src/app/game/game.component.spec.ts
+++ web/src/app/game/game.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameComponent } from './game.component';
@@ -8,7 +9,8 @@ describe('GameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameComponent ]
+      declarations: [ GameComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
 
--- web/src/app/statistics/statistics.component.spec.ts
+++ web/src/app/statistics/statistics.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatisticsComponent } from './statistics.component';
@@ -8,7 +9,8 @@ describe('StatisticsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StatisticsComponent ]
+      declarations: [ StatisticsComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
 
